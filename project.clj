28520b510c24b6c82126bfb8f3b7e504ea0e8f99
Alex Memory <alexmemory@gmail.com>
2017-04-11 10:17:18 -0700

    Update dependencies to other packages

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.alexmemory/clj-util "0.1.0-SNAPSHOT"
+  :description "Utilities for Clojure"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.395"] 
+                 ])
