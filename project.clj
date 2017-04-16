85bf0c69e36fc499705104ff59e2cf3630288031
Alex Memory <alexmemory@gmail.com>
2017-04-16 07:43:51 -0700

    URL in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.clojars.alexmemory/clj-util "0.1.0"
   :description "Utilities for Clojure"
+  :url "https://github.com/alexmemory/clj-util"
   :dependencies [
                  [clj-time "0.11.0"]
                  [org.clojure/clojure "1.7.0"]
