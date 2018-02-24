59fbae3df3a316ee394d1e29726712f4422a0eb0
Alex Memory <alexmemory@gmail.com>
2018-02-24 07:51:35 -0800

    Use Clojure 1.8.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.alexmemory/clj-util "0.1.0"
+(defproject org.clojars.alexmemory/clj-util "0.1.1-SNAPSHOT"
   :description "Utilities for Clojure"
   :url "https://github.com/alexmemory/clj-util"
   :dependencies [
                  [clj-time "0.11.0"]
-                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.395"] 
                  ]
   :aot :all
